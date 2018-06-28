d10346f468b6c2d8ae792013c7fb40c413a72eee
ganesa-vijayakumar <ganeshnet.vgp@gmail.com>
2018-06-29 00:08:10 +0530

    Initial commit

---
+++ src/CheckIfAStringIsColindrome.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * Check if a string is Colindrome Given a string, check if it is Colindrome or
+ * not. A string is said to be colindrome if it has consecutive 3 alphabets
+ * followed by the reverse of these 3 alphabets and so on.
+ * 
+ * Examples :
+ * 
+ * Input : cappaccappac Output : String is colindrome
+ * 
+ * Input : mollomaappaa Output : String is Colindrome
+ * 
+ * Approach : Take two empty strings s1 and s2, and start iterating over the
+ * given string. Take first three letters of the string and store it in s1, next
+ * three chars in s2 and then match s1 and s2. If they are same then do the same
+ * again till length of the given string else return false.
+ * 
+ */
+public class CheckIfAStringIsColindrome {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/ProblemAndSoluction1.java
@@ -0,0 +1,27 @@
+/**
+ * Find the number of operations required to make all array elements Equal Given
+ * an array of N integers, the task is to find the number of operations required
+ * to make all elements in the array equal. In one operation we can distribute
+ * equal weights from the maximum element to the rest of the array elements. If
+ * it is not possible to make the array elements equal after performing the
+ * above operations then print -1.<br>
+ * Examples:<br>
+ * 
+ * Input: arr = [1, 6, 1, 1, 1];<br>
+ * Output: 4<br>
+ * Explanation: Since arr becomes [2, 2, 2, 2, 2] after distribution from max
+ * element.<br>
+ * <br>
+ * 
+ * Input : arr = [2, 2, 3];<br>
+ * Output : -1<br>
+ * Explanation: Here arr becomes [3, 3, 1] after distribution.
+ **/
+
+public class ProblemAndSoluction1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
